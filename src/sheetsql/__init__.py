ace80b0c98b70de43e0f9c0a342089289d0837bf
Andy Huynh <andy.huynh312@gmail.com>
2020-06-28 22:15:45 -0700

    Make poetry single source of truth for versions

--- src/sheetsql/__init__.py
+++ src/sheetsql/__init__.py
@@ -1,10 +1,16 @@
 """SQL for Google Sheets."""
-
-__version__ = "0.1.0"
-
+try:
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
 from typing import Any
 
 from .connection import GoogleSheetsConnection
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = "unknown"
 
 
 def connect(auth_type: str, **kwargs: Any) -> GoogleSheetsConnection:
